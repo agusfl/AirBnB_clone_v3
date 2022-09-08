446bedd07cb319095ae7cbea9c2547b373f8c526
agusfl <agusflom@hotmail.com>
2022-09-08 14:44:04 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,9 +8,19 @@ Requirements:
 """
 from api.v1.views import app_views
 from flask import jsonify  # convert to JSON data
+from storage import count
+from models import Amenity, City, Place, Review, State, User
 
 
 @app_views.route("/status", methods=['GET'])
 def return_status():
     """Return status of GET request"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats", methods=['GET'])
+def return_stats():
+    """Return the number of each object by type"""
+    return jsonify({'amenities': count(Amenity), 'cities': count(City),
+                    'places': count(Place), 'reviews': count(Review),
+                    'states': count(State), 'users': count(User)})
