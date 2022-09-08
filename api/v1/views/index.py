19d43196964eae8c5a1f15fa2cc4ba71fdbb56fb
agusfl <agusflom@hotmail.com>
2022-09-08 13:23:17 +0000

    adding files for task 4

---
+++ api/v1/views/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Requirements:
+
+- import app_views from api.v1.views
+- create a route /status on the object app_views that returns a JSON:
+"status": "OK" (see example)
+"""
+from api.v1.views import app_views
+from flask import jsonify  # convert to JSON data
+
+@app_views.route("/status", methods=['GET'])
+def return_status():
+    """Return status of GET request"""
+    return jsonify({'status': 'OK'})
