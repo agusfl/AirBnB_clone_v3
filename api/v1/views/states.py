a26fb801318b11d8351db26f549a320e6c4c0fca
agusfl <agusflom@hotmail.com>
2022-09-08 18:45:40 +0000

    adding GET method of states - task 7

---
+++ api/v1/views/states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Create a new view for State objects that handles all default RESTFul API
+actions.
+"""
+from api.v1.views import app_views
+from flask import jsonify  # convert to JSON data
+from models import storage
+from models.state import State
+from flask import abort
+
+
+@app_views.route("/states", methods=['GET'])
+def return_states():
+    """
+    Return states - use GET request
+    No es necesario poner la opcion methods aca ya que GET se hace por default,
+    lo ponemos por mayor claridad nomas.
+    """
+    states = storage.all(State)
+    return jsonify({states.to_dict()})
