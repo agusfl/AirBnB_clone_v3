6534eeab93b40ca667725e0e962d9ddfaec050ec
agusfl <agusflom@hotmail.com>
2022-09-08 14:17:41 +0000

    fix style

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
-# create a variable app_views which is an instance of Blueprint 
-# (url prefix must be /api/v1)
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 # Import flask views:
 from api.v1.views.index import *
+# create a variable app_views which is an instance of Blueprint
+# (url prefix must be /api/v1)
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
