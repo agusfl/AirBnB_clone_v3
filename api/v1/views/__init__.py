5979d06f354da0ba8a2b05ffb18cfbed6cf9f1bf
agusfl <agusflom@hotmail.com>
2022-09-08 13:01:46 +0000

    adding files for task 4

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+# create a variable app_views which is an instance of Blueprint 
+# (url prefix must be /api/v1)
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# Import flask views:
+from api.v1.views.index import *
