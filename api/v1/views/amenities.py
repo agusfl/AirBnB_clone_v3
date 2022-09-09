ed657c2fe5c0918a3337ca77c48da9f98755ee04
agusfl <agusflom@hotmail.com>
2022-09-09 18:05:09 +0000

    adding PUT method for task 9 (amenities)

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -107,3 +107,37 @@ def post_amenity():
         storage.save()
         # Se devuelve el objeto creado y un status code de 201
         return make_response(jsonify(obj.to_dict()), 201)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_amenities_id(amenity_id):
+    """
+    Make a POST request HTTP to update data.
+    """
+    # Hacemos la request de la data que se pase en formato json y la
+    # pasamos a un dic de python para poder trabajar con ella
+    body = request.get_json()
+
+    if body is None:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+    # Traemos todos los objetos de la clase State que esten en el storage
+    amenity = storage.get(Amenity, amenity_id)
+
+    if amenity is None:
+        # Se usa el metodo abort de flask en caso que no se pase una ID
+        abort(404)
+    else:
+        # keys to ignore - not change
+        keys_ignore = ["id", "created_at", "updated_at"]
+
+        for key, value in body.items():
+            if key not in keys_ignore:
+                setattr(amenity, key, value)
+            else:
+                pass
+        # Se guarda el nuevo objeto dentro del storage
+        storage.save()
+        # Se devuelve el objeto creado y un status code de 200
+        return make_response(jsonify(amenity.to_dict()), 200)
