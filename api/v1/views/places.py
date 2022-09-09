d3362e8cfb45bddff06c65ef69faec7737d0ef96
agusfl <agusflom@hotmail.com>
2022-09-09 18:53:36 +0000

    adding GET method to palces endpoint

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -33,3 +33,21 @@ def return_places(city_id):
     for place in city.cities:
         cities.append(place.to_dict())
     return jsonify(cities)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
+def return_places_id(place_id):
+    """
+    Return state objects by id or 404 if the id does not exists
+    """
+    # Traemos el objeto especifico de place por id con el metódo get
+    # Creado en DBStorage
+    place = storage.get(Place, place_id)
+
+    # Si place llega vacío
+    if place is None:
+        # Se usa el metodo abort de flask en caso que no se encuentre la ID
+        abort(404)
+    # de lo contrario devolvemos el objeto pasado a json:
+    return jsonify(place.to_dict())
