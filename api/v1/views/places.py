ea6d8b4a0b5a958aea6e0686bf70875e7c316a9d
agusfl <agusflom@hotmail.com>
2022-09-09 18:45:18 +0000

    adding GET method to palces endpoint

---
+++ api/v1/views/places.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Create a new view for Place objects that handles all default RESTFul API
+actions.
+"""
+from api.v1.views import app_views
+from flask import jsonify  # convert to JSON data
+from models import storage
+from models.place import Place
+from flask import abort
+from flask import make_response  # errorhandler(404)
+from flask import request  # for get_json()
+
+
+@app_views.route("/cities/<city_id>/places", strict_slashes=False,
+                 methods=['GET'])
+def return_cities(city_id):
+    """
+    Return places - use GET request
+    Cada lugar (places) esta directamente relacionado con la ciudad, no puede
+    haber lugares sin ciudades.
+    """
+    # Traemos el objeto especifico de city por id con el metódo get
+    # creado en DBStorage
+    city = storage.get(City, city_id)
+
+    # Si city llega vacío
+    if city is None:
+        # Se usa el metodo abort de flask en caso que no se encuentre la ID
+        abort(404)
+    places = []
+    for place in city.cities:
+        cities.append(place.to_dict())
+    return jsonify(cities)
