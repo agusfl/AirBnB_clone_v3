8894ad0c36b4830257241a5453864c0d90a27bf7
agusfl <agusflom@hotmail.com>
2022-09-10 21:30:26 +0000

    update comments

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Create a new view for Place objects that handles all default RESTFul API
-actions.
+Create a new view for Place objects that handles all default HTTP methods.
 """
 from api.v1.views import app_views
 from flask import jsonify  # convert to JSON data
@@ -18,7 +17,7 @@ from flask import request  # for get_json()
                  methods=['GET'])
 def return_places(city_id):
     """
-    Return places - use GET request
+    Return places - use GET request.
     Cada lugar (places) esta directamente relacionado con la ciudad, no puede
     haber lugares sin ciudades.
     """
@@ -26,7 +25,7 @@ def return_places(city_id):
     # creado en DBStorage
     city = storage.get(City, city_id)
 
-    # Si city llega vacío
+    # If the city_id is not linked to any City object, raise a 404 error
     if city is None:
         # Se usa el metodo abort de flask en caso que no se encuentre la ID
         abort(404)
@@ -40,17 +39,15 @@ def return_places(city_id):
                  strict_slashes=False)
 def return_places_id(place_id):
     """
-    Return place objects by id or 404 if the id does not exists
+    Return place objects by id or 404 if the id does not exists.
     """
     # Traemos el objeto especifico de place por id con el metódo get
-    # Creado en DBStorage
+    # creado en DBStorage.
     place = storage.get(Place, place_id)
 
-    # Si place llega vacío
+    # If the place_id is not linked to any Place object, raise a 404 error
     if place is None:
-        # Se usa el metodo abort de flask en caso que no se encuentre la ID
         abort(404)
-    # de lo contrario devolvemos el objeto pasado a json:
     return jsonify(place.to_dict())
 
 
@@ -58,8 +55,7 @@ def return_places_id(place_id):
                  strict_slashes=False)
 def delete_places_id(place_id):
     """
-    If the place_id is not linked to any Place object, raise a 404 error
-    Returns an empty dictionary with the status code 200
+    Delete a Place object by id.
     """
     # Se trae el objeto Place que se pase la "id"
     place = storage.get(Place, place_id)
@@ -80,13 +76,7 @@ def delete_places_id(place_id):
                  methods=['POST'])
 def post_place(city_id):
     """
-    - You must use request.get_json from Flask to transform the HTTP body
-    request to a dictionary
-    - If the HTTP body request is not valid JSON, raise a 400 error with the
-    message Not a JSON
-    - If the dictionary doesn’t contain the key name, raise a 400 error with
-    the message Missing name
-    - Returns the new State with the status code 201
+    Create a Place object.
     """
     # Hacemos la request de la data que se pase en formato json y la
     # pasamos a un dic de python para poder trabajar con ella
@@ -96,7 +86,7 @@ def post_place(city_id):
     if body is None:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
 
-    # Traemos state por su "id"
+    # Traemos City object por su "id"
     city = storage.get(City, city_id)
 
     # If the city_id is not linked to any City object, raise a 404 error
@@ -142,7 +132,7 @@ def post_place(city_id):
                  strict_slashes=False)
 def update_places_id(place_id):
     """
-    Make a POST request HTTP to update data.
+    Make a PUT request HTTP to update data.
     """
     # Hacemos la request de la data que se pase en formato json y la
     # pasamos a un dic de python para poder trabajar con ella
